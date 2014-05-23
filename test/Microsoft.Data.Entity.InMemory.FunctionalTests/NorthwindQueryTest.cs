9e84b69bbe53d8c02b0f235a17872c8df15aa685
ajcvickers <ajcvickers@hotmail.com>
2014-05-23 08:16:30 -0700

    Space-age composite polymer; not a shred of sqlmetal... (Support composite keys end-to-end)
    This change adds support for value generation with composite keys which was blocking use of composite keys end-to-end. The state entry will now generate values for any property marked to have value generation on add regardless of whether that property is part of a key, composite or not.

--- test/Microsoft.Data.Entity.InMemory.FunctionalTests/NorthwindQueryTest.cs
+++ test/Microsoft.Data.Entity.InMemory.FunctionalTests/NorthwindQueryTest.cs
@@ -40,7 +40,7 @@ namespace Microsoft.Data.Entity.InMemory.FunctionalTests
                 context.Set<Employee>().AddRange(NorthwindData.Employees);
                 context.Set<Order>().AddRange(NorthwindData.Orders);
                 context.Set<Product>().AddRange(NorthwindData.Products);
-                //context.Set<OrderDetail>().AddRange(NorthwindData.OrderDetails); // composite keys
+                context.Set<OrderDetail>().AddRange(NorthwindData.OrderDetails);
                 context.SaveChanges();
             }
         }
